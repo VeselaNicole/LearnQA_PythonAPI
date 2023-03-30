3fb6fbf22b48f982f37bd0c13a5a8c6784a8aba7
VeselaNicole <56206131+VeselaNicole@users.noreply.github.com>
2023-03-30 15:26:43 +0300

    Ex.5

--- json_parsing.py
+++ json_parsing.py
@@ -1,6 +1,6 @@
 import json
 from json.decoder import JSONDecodeError
-json_text = '"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 key_1 = "messages"
 key_2 = "message"
 try:
